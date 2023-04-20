411cffacf50f111bcef70291d9b5011e8ed5b2f4
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-04-20 14:01:15 +0300

    finish header

--- components/CommonUIComponents/HeaderWithIcons.tsx
+++ components/CommonUIComponents/HeaderWithIcons.tsx
@@ -9,26 +9,65 @@
 import React from "react";
 import {Block} from "../../styles/Block";
 import {Text} from "react-native";
+import {Image} from "../../styles/Image";
+
+import bluetoothImage from '../../assets/bluetooth.png';
+import wifiImage from '../../assets/wi-fi.png';
+import cloud from '../../assets/cloud.png';
 
 const HeaderWithIcons = () => {
 
     return (
         <Block
-            height={'150px'}
         >
             <Block
                 flexDirection={'row'}
-                justifyContent={'space-around'}
+                justifyContent={'space-between'}
                 paddingVertical={'10px'}
+                paddingHorizontal={'10px'}
             >
-                <Block >
+                <Block
+                    flexDirection={'row'}
+                    alignItems={'center'}
+                    width={'30%'}
+                >
+                    <Image
+                        resizeMode={'contain'}
+                        width='50px'
+                        height='50px'
+                        onError={() => console.log('error')}
+                        onLoad={() => console.log('loaded')}
+                        source={bluetoothImage}
+                    />
                     <Text>100%</Text>
                 </Block>
-                <Block>
-                    <Text>WiFi</Text>
+                <Block
+                    alignItems={'center'}
+                    justifyContent={'center'}
+                    width={'35%'}
+                >
+                    <Image
+                        resizeMode={'contain'}
+                        width='35px'
+                        height='35px'
+                        onError={() => console.log('error')}
+                        onLoad={() => console.log('loaded')}
+                        source={wifiImage}
+                    />
                 </Block>
-                <Block>
-                    <Text>Cloud</Text>
+                <Block
+                    alignItems={'center'}
+                    justifyContent={'center'}
+                    width={'30%'}
+                >
+                    <Image
+                        resizeMode={'contain'}
+                        width='40px'
+                        height='40px'
+                        onError={() => console.log('error')}
+                        onLoad={() => console.log('loaded')}
+                        source={cloud}
+                    />
                 </Block>
             </Block>
 
