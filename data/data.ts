a2db74dcc4be91aff3048a7ac0fe1ab2b5ae27cb
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-04-20 17:23:31 +0300

    added article section

---
+++ data/data.ts
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id: 1,
+        code: 'CG0934',
+        color: 'White',
+        size: 'XS',
+        description: 'Longsleeve Women X',
+        quantity: 3
+    },
+    {
+        id: 2,
+        code: 'AC8435',
+        color: 'White',
+        size: 'S',
+        description: 'Longsleeve Women Y',
+        quantity: 3
+    },
+    {
+        id: 3,
+        code: 'CG0935',
+        color: 'White',
+        size: 'XL',
+        description: 'Longsleeve Women Z',
+        quantity: 3
+    },
+];
+
+export default data;
