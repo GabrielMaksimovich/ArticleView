c5c472e55778827fec7f6434e3d1a4ccdabfd9ac
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-04-21 20:41:41 +0300

    refactor code, added type, and added new component ListItem

--- components/SimpleComponents/ArticleSection.tsx
+++ components/SimpleComponents/ArticleSection.tsx
@@ -1,11 +1,8 @@
 import React, { FC, useState } from "react";
-import { Animated, Text, TouchableOpacity } from "react-native";
+import { Animated } from "react-native";
 import { Block } from "../../styles/Block";
-import { Image } from "../../styles/Image";
-import { Button } from "../../styles/Button";
-import longsleeve from "../../assets/longsleeve.webp";
-import rightArrow from "../../assets/right-arrow.png";
 import data from "../../data/data";
+import ListItem from "./ListItem";
 
 const ArticleSection: FC = () => {
     const [slideAnimations] = useState(() => data.map(() => new Animated.Value(0)));
@@ -71,104 +68,15 @@ const ArticleSection: FC = () => {
     return (
         <Block>
             {data.map((item, index) => (
-                <Block
+                <ListItem
                     key={item.id}
-                    borderBottomColor={"grey"}
-                    borderBottomWidth={"0.5px"}
-                >
-                    <Animated.View
-                        style={{
-                            transform: [
-                                {translateX: slideAnimations[index]},
-                                {scaleX: 0.9},
-                            ],
-                        }}
-                    >
-                        <Block
-                            flexDirection={"row"}
-                            alignItems={"center"}
-                            justifyContent={"space-between"}
-                            paddingVertical={"10px"}
-                        >
-                            <Block flexDirection={"row"} alignItems={"center"}>
-                                <Animated.View style={{ opacity: imageOpacities[index] }}>
-                                    <Image
-                                        resizeMode={"contain"}
-                                        width="80px"
-                                        height="80px"
-                                        onError={() => console.log("error")}
-                                        onLoad={() => console.log("loaded")}
-                                        source={longsleeve}
-                                    />
-                                </Animated.View>
-
-                                <Block>
-                                    <Text style={{ marginBottom: 5, fontSize: 10 }}>{item.code}</Text>
-                                    <Block
-                                        flexDirection={"row"}
-                                        justifyContent={"flex-start"}
-                                        marginBottom={5}
-                                    >
-                                        <Text style={{ marginRight: 30, fontSize: 10 }}>{item.color}</Text>
-                                        <Text style={{ fontSize: 10 }}>{item.size}</Text>
-                                    </Block>
-                                    <Text style={{ color: "#9e9e9e", fontSize: 10 }}>
-                                        {item.description}
-                                    </Text>
-                                </Block>
-                            </Block>
-
-
-                                <Block
-                                    flexDirection={"row"}
-                                    alignItems={"center"}
-                                    justifyContent={"space-around"}
-                                >
-                                    <Block marginRight={10}>
-                                        <Button
-                                            onPress={() => 'clicked'}
-                                            width={"80px"}
-                                            height={"15px"}
-                                            bg={"green"}
-                                            borderRadius={"10px"}
-                                            justifyContent={"center"}
-                                            alignItems={"center"}
-                                        >
-                                            <Text style={{ color: "#fff", fontSize: 12 }}>{item.quantity}</Text>
-                                        </Button>
-                                    </Block>
-
-                                    <TouchableOpacity onPress={() => handlePress(item.id)}>
-                                        <Image
-                                            resizeMode={"contain"}
-                                            width="10px"
-                                            height="10px"
-                                            onError={() => console.log("error")}
-                                            onLoad={() => console.log("loaded")}
-                                            source={rightArrow}
-                                        />
-                                    </TouchableOpacity>
-                                </Block>
-                        </Block>
-                    </Animated.View>
-
-                    {showDeleteButton === item.id && (
-                        <Block
-                            bg={"red"}
-                            justifyContent={"center"}
-                            alignItems={"center"}
-                            height={"100%"}
-                            width={"50px"}
-                            position={"absolute"}
-                            top={"0"}
-                            right={"0"}
-                        >
-                            <Button onPress={handleDelete}>
-                                <Text style={{ color: "#fff" }}>Delete</Text>
-                            </Button>
-                        </Block>
-                    )}
-                </Block>
+                    item={item}
+                    handlePress={handlePress}
+                    handleDelete={handleDelete}
+                    slideAnimation={slideAnimations[index]}
+                    imageOpacity={imageOpacities[index]}
+                    showDeleteButton={showDeleteButton}
+                />
             ))}
         </Block>
     );
---
+++ components/SimpleComponents/ListItem.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import { Text, TouchableOpacity, Animated } from 'react-native';
+import { Block } from '../../styles/Block';
+import { Image } from '../../styles/Image';
+import { Button } from '../../styles/Button';
+import longsleeve from '../../assets/longsleeve.webp';
+import rightArrow from '../../assets/right-arrow.png';
+import DataItem from "../../types/DataItem";
+
+type Props = {
+    item: DataItem;
+    handlePress: (itemId: number | null) => void;
+    handleDelete: () => void;
+    slideAnimation: Animated.Value;
+    imageOpacity: Animated.Value;
+    showDeleteButton: number | null;
+};
+
+const ListItem: React.FC<Props> = ({
+       item,
+       handlePress,
+       handleDelete,
+       slideAnimation,
+       imageOpacity,
+       showDeleteButton,
+    }) => {
+    return (
+        <Block
+            borderBottomColor={"grey"}
+            borderBottomWidth={"0.5px"}
+        >
+            <Animated.View
+                style={{
+                    transform: [
+                        {translateX: slideAnimation},
+                        {scaleX: 0.9},
+                    ],
+                }}
+            >
+                <Block
+                    flexDirection={"row"}
+                    alignItems={"center"}
+                    justifyContent={"space-between"}
+                    paddingVertical={"10px"}
+                >
+                    <Block flexDirection={"row"} alignItems={"center"}>
+                        <Animated.View style={{ opacity: imageOpacity}}>
+                            <Image
+                                resizeMode={"contain"}
+                                width="80px"
+                                height="80px"
+                                onError={() => console.log("error")}
+                                onLoad={() => console.log("loaded")}
+                                source={longsleeve}
+                            />
+                        </Animated.View>
+
+                        <Block>
+                            <Text style={{ marginBottom: 5, fontSize: 10 }}>{item.code}</Text>
+                            <Block
+                                flexDirection={"row"}
+                                justifyContent={"flex-start"}
+                                marginBottom={5}
+                            >
+                                <Text style={{ marginRight: 30, fontSize: 10 }}>{item.color}</Text>
+                                <Text style={{ fontSize: 10 }}>{item.size}</Text>
+                            </Block>
+                            <Text style={{ color: "#9e9e9e", fontSize: 10 }}>
+                                {item.description}
+                            </Text>
+                        </Block>
+                    </Block>
+
+
+                    <Block
+                        flexDirection={"row"}
+                        alignItems={"center"}
+                        justifyContent={"space-around"}
+                    >
+                        <Block marginRight={10}>
+                            <Button
+                                onPress={() => 'clicked'}
+                                width={"80px"}
+                                height={"15px"}
+                                bg={"green"}
+                                borderRadius={"10px"}
+                                justifyContent={"center"}
+                                alignItems={"center"}
+                            >
+                                <Text style={{ color: "#fff", fontSize: 12 }}>{item.quantity}</Text>
+                            </Button>
+                        </Block>
+
+                        <TouchableOpacity onPress={() => handlePress(item.id)}>
+                            <Image
+                                resizeMode={"contain"}
+                                width="10px"
+                                height="10px"
+                                onError={() => console.log("error")}
+                                onLoad={() => console.log("loaded")}
+                                source={rightArrow}
+                            />
+                        </TouchableOpacity>
+                    </Block>
+                </Block>
+            </Animated.View>
+
+            {showDeleteButton === item.id && (
+                <Block
+                    bg={"red"}
+                    justifyContent={"center"}
+                    alignItems={"center"}
+                    height={"100%"}
+                    width={"50px"}
+                    position={"absolute"}
+                    top={"0"}
+                    right={"0"}
+                >
+                    <Button onPress={handleDelete}>
+                        <Text style={{ color: "#fff" }}>Delete</Text>
+                    </Button>
+                </Block>
+            )}
+        </Block>
+    );
+};
+
+export default ListItem;
