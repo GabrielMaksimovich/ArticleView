3416b824113a018b1192b762875ca61c79a10257
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-05-04 18:08:08 +0300

    fix all issues but got white screen

--- screens/Scanner.tsx
+++ screens/Scanner.tsx
@@ -1,11 +1,64 @@
+import React from "react";
+import {Camera, useCameraDevices } from 'react-native-vision-camera';
 import {Block} from "../components/SimpleComponents/Block";
-import {Text} from "../components/SimpleComponents/Text";
-
+import {Linking, View} from "react-native";
 
 const Scanner = () => {
+    const [isDeviceReady, setIsDeviceReady] = React.useState(false);
+
+    //Camera
+    const devices = useCameraDevices();
+    const device = devices.back;
+
+    const requestCameraPermission = React.useCallback(async () => {
+        const permission = await Camera.requestCameraPermission();
+
+        if (permission === 'denied') await Linking.openSettings()
+    }, []);
+
+    React.useEffect(() => {
+        try {
+            requestCameraPermission().then(() => {
+                setIsDeviceReady(true);
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }, []);
+
+
+    //Handler
+    function renderCamera() {
+        if (!isDeviceReady || device == null) {
+            return (
+                <View
+                    style={{
+                        flex: 1,
+                    }}
+                />
+            );
+        } else {
+            return (
+                <View
+                    style={{
+                        flex: 1,
+                    }}
+                >
+                    <Camera
+                        style={{ flex: 1 }}
+                        device={device}
+                        isActive={true}
+                        enableZoomGesture
+                    />
+                </View>
+            );
+        }
+    }
+
+
     return (
         <Block flex={1} alignItems={'center'} justifyContent={'center'}>
-            <Text>Hello</Text>
+            {renderCamera()}
         </Block>
     );
 };
