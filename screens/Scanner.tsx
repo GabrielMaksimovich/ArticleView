9569a8db2f46dad72d8b56ee61d893a68814ae20
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-05-08 11:01:38 +0300

    added initial code

--- screens/Scanner.tsx
+++ screens/Scanner.tsx
@@ -1,86 +1,73 @@
-import React from "react";
-import { Camera, useCameraDevices } from "react-native-vision-camera";
-import { Linking, View, StyleSheet } from "react-native";
-import { Platform, PermissionsAndroid } from "react-native";
+import React, { useRef } from 'react';
+import {Alert, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+    Camera,
+    CameraType,
+} from 'react-native-camera-kit';
+import RNFS from 'react-native-fs';
+import { check, PERMISSIONS, request } from 'react-native-permissions';
 
-const Scanner = () => {
-    const [isDeviceReady, setIsDeviceReady] = React.useState(false);
+const Scanner: React.FC = () => {
+    const cameraRef = useRef<Camera>(null);
 
-    // Camera
-    const devices = useCameraDevices();
-    const device = devices.back;
-
-    const requestCameraPermission = React.useCallback(async () => {
-        try {
-            if (Platform.OS === "android") {
-                const granted = await PermissionsAndroid.request(
-                    PermissionsAndroid.PERMISSIONS.CAMERA,
-                    {
-                        title: "Camera Permission",
-                        message: "This app needs camera access",
-                        buttonNeutral: "Ask Me Later",
-                        buttonNegative: "Cancel",
-                        buttonPositive: "OK",
-                    }
-                );
-                if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                    setIsDeviceReady(true);
-                } else {
-                    console.log("Camera permission denied");
-                }
-            } else {
-                const permission = await Camera.requestCameraPermission();
-                if (permission === "authorized") {
-                    setIsDeviceReady(true);
-                } else {
-                    console.log("Camera permission denied");
-                }
-            }
-        } catch (err) {
-            console.warn(err);
+    const checkAndRequestPermissions = async () => {
+        const cameraPermission = await check(PERMISSIONS.ANDROID.CAMERA);
+        if (cameraPermission !== 'granted') {
+            await request(PERMISSIONS.ANDROID.CAMERA);
         }
-    }, []);
+    };
+
+    const handleCapture = async () => {
+        if (cameraRef.current) {
+            const { uri } = await cameraRef.current.capture();
+            const filePath = uri.replace('file://', '');
+            const pathSegments = filePath.split('/');
+            const fileName = pathSegments[pathSegments.length - 1];
+            const destFilePath = `${RNFS.DocumentDirectoryPath}/${fileName}`;
+            await RNFS.moveFile(filePath, destFilePath);
+            Alert.alert('Photo saved at', destFilePath);
+        }
+    };
+
+    const handleReadCode = (event: any) => {
+        Alert.alert('QR code found', event.nativeEvent.codeStringValue);
+    };
 
     React.useEffect(() => {
-        try {
-            requestCameraPermission().then(() => {
-                setIsDeviceReady(true);
-            });
-        } catch (e) {
-            console.log(e);
-        }
+        checkAndRequestPermissions();
     }, []);
 
-    // Handler
-    function renderCamera() {
-        if (!isDeviceReady || device == null) {
-            console.log("Devices: ", devices);
-            return <View style={styles.container} />;
-        } else {
-            return (
-                <View style={styles.container}>
-                    <Camera
-                        style={styles.camera}
-                        device={device}
-                        isActive={true}
-                        enableZoomGesture
-                    />
-                </View>
-            );
-        }
-    }
-
-    return <View style={styles.container}>{renderCamera()}</View>;
+    return (
+        <View style={styles.container}>
+            <Camera
+                ref={cameraRef}
+                style={styles.camera}
+                cameraType={CameraType.Back}
+                // Add barcode scanning props
+                scanBarcode={true}
+                onReadCode={handleReadCode}
+                showFrame={true}
+                laserColor="red"
+                frameColor="white"
+            />
+            <TouchableOpacity style={styles.captureButton} onPress={handleCapture}>
+                <Text>Capture</Text>
+            </TouchableOpacity>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
     },
     camera: {
         flex: 1,
+    },
+    captureButton: {
+        position: 'absolute',
+        bottom: 30,
+        alignSelf: 'center',
     },
 });
 
