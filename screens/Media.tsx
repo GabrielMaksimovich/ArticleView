50cd91ec0e288d5560ef060b6ee0777d7e7fb7eb
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-05-18 11:37:15 +0300

    added next\previous buttons

--- screens/Media.tsx
+++ screens/Media.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import TrackPlayer, { useProgress, Capability } from 'react-native-track-player';
-import {Text, View, Button} from 'react-native';
+import { Text, View, Button } from 'react-native';
 
 const MusicPlayer = () => {
     useEffect(() => {
@@ -20,20 +20,53 @@ const MusicPlayer = () => {
             compactCapabilities: [
                 Capability.Play,
                 Capability.Pause,
+                Capability.SkipToNext,
+                Capability.SkipToPrevious,
             ],
         });
+        await TrackPlayer.add([
+            {
+                id: '1',
+                url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+                title: 'Test Track',
+                artist: 'Test Artist',
+                artwork: 'https://example.com/cover.png',
+            },
+            {
+                id: '2',
+                url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+                title: 'Test Track 2',
+                artist: 'Test Artist',
+                artwork: 'https://example.com/cover.png',
+            },
+            {
+                id: '3',
+                url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3',
+                title: 'Test Track 3',
+                artist: 'Test Artist',
+                artwork: 'https://example.com/cover.png',
+            },
+        ]);
     };
 
     const startPlayback = async () => {
-        // You should replace this URL with your own audio file
-        await TrackPlayer.add({
-            id: '1',
-            url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
-            title: 'Test Track',
-            artist: 'Test Artist',
-            artwork: 'https://example.com/cover.png',
-        });
         await TrackPlayer.play();
+    };
+
+    const pausePlayback = async () => {
+        await TrackPlayer.pause();
+    };
+
+    const stopPlayback = async () => {
+        await TrackPlayer.reset();
+    };
+
+    const skipToNext = async () => {
+        await TrackPlayer.skipToNext();
+    };
+
+    const skipToPrevious = async () => {
+        await TrackPlayer.skipToPrevious();
     };
 
     const { position, buffered, duration } = useProgress();
@@ -41,6 +74,10 @@ const MusicPlayer = () => {
     return (
         <View>
             <Button title="Play Track" onPress={startPlayback} />
+            <Button title="Pause Track" onPress={pausePlayback} />
+            <Button title="Stop Track" onPress={stopPlayback} />
+            <Button title="Next Track" onPress={skipToNext} />
+            <Button title="Previous Track" onPress={skipToPrevious} />
             <Text>Current Position: {position}</Text>
             <Text>Buffered Position: {buffered}</Text>
             <Text>Duration: {duration}</Text>
--- service.js
+++ service.js
@@ -1,4 +1,23 @@
+import TrackPlayer from 'react-native-track-player';
+
 module.exports = async function() {
-    // This service needs to be registered for the module to work
-    // but it will be used later in the "Receiving Events" section
-}
+    TrackPlayer.addEventListener('remote-play', () => {
+        TrackPlayer.play();
+    });
+
+    TrackPlayer.addEventListener('remote-pause', () => {
+        TrackPlayer.pause();
+    });
+
+    TrackPlayer.addEventListener('remote-next', async () => {
+        await TrackPlayer.skipToNext();
+    });
+
+    TrackPlayer.addEventListener('remote-previous', async () => {
+        await TrackPlayer.skipToPrevious();
+    });
+
+    TrackPlayer.addEventListener('remote-stop', () => {
+        TrackPlayer.reset();
+    });
+};
