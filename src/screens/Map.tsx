c809e309d0de5b765b054c72ffcdcab03a53cbc3
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-06-07 11:34:36 +0300

    added styles and components

--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -2,10 +2,14 @@ import React, {useEffect, useState} from 'react';
 import MapView, {Polyline, LatLng, PROVIDER_GOOGLE} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 import firestore from '@react-native-firebase/firestore';
-import {PermissionsAndroid, Platform} from 'react-native';
+import { PermissionsAndroid, Platform } from 'react-native';
+import {Block} from "../components/SimpleComponents/Block";
+import {Button} from "../components/SimpleComponents/Button";
+import {Text} from "../components/SimpleComponents/Text";
 
 const MapTracker: React.FC = () => {
     const [route, setRoute] = useState<LatLng[]>([]);
+    const [watchId, setWatchId] = useState<number | null>(null);
 
     const requestLocationPermission = async () => {
         if (Platform.OS === 'android') {
@@ -32,45 +36,80 @@ const MapTracker: React.FC = () => {
         return true;
     };
 
+    const startTracking = async () => {
+        if (!(await requestLocationPermission())) {
+            return;
+        }
+
+        const id = Geolocation.watchPosition(
+            (position) => {
+                const { latitude, longitude } = position.coords;
+                setRoute((currentRoute) => [...currentRoute, { latitude, longitude }]);
+
+                firestore()
+                    .collection('routes')
+                    .add({ latitude, longitude, timestamp: Date.now() });
+            },
+            (error) => console.log(error),
+            { distanceFilter: 10, interval: 1000, fastestInterval: 500 }
+        );
+
+        setWatchId(id);
+    };
+
+    const stopTracking = () => {
+        if (watchId !== null) {
+            Geolocation.clearWatch(watchId);
+            setWatchId(null);
+        }
+    };
+
     useEffect(() => {
-        const startTracking = async () => {
-            if (!(await requestLocationPermission())) {
-                return;
-            }
-
-            const watchId = Geolocation.watchPosition(
-                (position) => {
-                    const { latitude, longitude } = position.coords;
-                    setRoute((currentRoute) => [...currentRoute, { latitude, longitude }]);
-
-                    firestore()
-                        .collection('routes')
-                        .add({ latitude, longitude, timestamp: Date.now() });
-                },
-                (error) => console.log(error),
-                { distanceFilter: 10, interval: 1000, fastestInterval: 500 }
-            );
-
-            return () => Geolocation.clearWatch(watchId);
-        };
-
         startTracking();
+
+        return () => {
+            stopTracking();
+        };
     }, []);
 
     return (
-        <MapView
-            provider={PROVIDER_GOOGLE}
-            style={{ flex: 1 }}
-            showsMyLocationButton={true}
-            initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-            }}
-        >
-            <Polyline coordinates={route} />
-        </MapView>
+        <Block flex={1}>
+            <MapView
+                provider={PROVIDER_GOOGLE}
+                style={{ flex: 1 }}
+                showsMyLocationButton={true}
+                followsUserLocation={true}
+                showsUserLocation={true}
+            >
+                <Polyline coordinates={route} />
+            </MapView>
+            <Block
+                flexDirection={'row'}
+                justifyContent={'space-between'}
+                alignItems={'flex-end'}
+                paddingHorizontal={20}
+                paddingVertical={20}
+            >
+            <Button
+                onPress={startTracking}
+                paddingVertical={10}
+                paddingHorizontal={10}
+                borderRadius={'25px'}
+                bg={'rgba(0, 0, 0, 0.5)'}
+            >
+                <Text color={'#fff'}>Start Tracking</Text>
+            </Button>
+            <Button
+                onPress={stopTracking}
+                paddingVertical={10}
+                paddingHorizontal={10}
+                borderRadius={'25px'}
+                bg={'rgba(0, 0, 0, 0.5)'}
+            >
+                <Text color={'#fff'}>Stop Tracking</Text>
+            </Button>
+            </Block>
+        </Block>
     );
 };
 
