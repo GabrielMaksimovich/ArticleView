a501f76f7ebb674ce811d8932230018c6cd59219
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-06-13 10:40:27 +0300

    simplify search

--- src/screens/GoogleSearchScreen.tsx
+++ src/screens/GoogleSearchScreen.tsx
@@ -1,24 +1,12 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
+import React from 'react';
 import { WebView } from 'react-native-webview';
+import {Block} from "../components/SimpleComponents/Block";
 
 const GoogleSearchScreen = () => {
-    const [searchTerm, setSearchTerm] = useState("");
-    const [searchUrl, setSearchUrl] = useState("https://www.google.com");
-
-    const searchGoogle = () => {
-        setSearchUrl("https://www.google.com/search?q=" + encodeURIComponent(searchTerm));
-    };
-
     return (
-        <View style={{flex: 1}}>
-            <TextInput
-                value={searchTerm}
-                onChangeText={setSearchTerm}
-            />
-            <Button title="Search Google" onPress={searchGoogle} />
-            <WebView source={{ uri: searchUrl }} style={{flex: 1}} />
-        </View>
+        <Block flex={1}>
+            <WebView source={{ uri: "https://www.google.com" }} style={{flex: 1}} />
+        </Block>
     );
 };
 
