3a5b5f731e382469b9372f763537301816520b66
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-06-14 20:02:59 +0300

    add screen for realm and check how its working

--- App.tsx
+++ App.tsx
@@ -19,6 +19,7 @@ import {PictureProvider} from "./PictureContext";
 import CameraStackNavigator from "./src/screens/navigation/CameraStackNavigator";
 import GoogleSearch from "./src/screens/GoogleSearch";
 import { RealmProvider } from './src/contexts/RealmContext';
+import RealmExampleScreen from "./src/screens/Realm";
 
 const Drawer = createDrawerNavigator();
 
@@ -53,6 +54,7 @@ const App = () => {
                                 <Drawer.Screen name="Media" component={Media} />
                                 <Drawer.Screen name="Camera" component={CameraStackNavigator} />
                                 <Drawer.Screen name="Google" component={GoogleSearch} />
+                                <Drawer.Screen name="Realm Example" component={RealmExampleScreen} />
                             </Drawer.Navigator>
                         </NavigationContainer>
                     </PictureProvider>
